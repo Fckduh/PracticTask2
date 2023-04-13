a070f5aa330dee122d63c96dbe33192585be48c1
Fckduh <115530809+Fckduh@users.noreply.github.com>
2023-04-13 16:20:34 +0500

    Add files via upload

---
+++ Practic2.0/PracticTask2.3/3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticTask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           bool prost = true;
+            Console.WriteLine("Введите простое число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i*i <= number ; i++)
+            {
+                if (number % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            if(prost)
+            {
+                Console.WriteLine("Число простое ");
+            }
+            else
+            {
+                Console.WriteLine("Число не простое ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practic2.0/PracticTask2.4/4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticTask2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int min = int.MaxValue;
+
+            Console.WriteLine("Введите целые числа последовательности (числа разделяются клавишей Enter):");
+            for (int i = 0; i < length; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < min) 
+                {
+                    min = num; 
+                }
+            }
+
+            Console.WriteLine($"Наименьший элемент в последовательности: {min}");
+            Console.ReadKey();
+        }
+    }
+}
